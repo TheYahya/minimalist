c9c24c4621b00978141480b7cfd3036997d16144
Yahya SayadArbabi <yahya.arbabi@gmail.com>
2016-12-05 09:36:19 +0330

    fix pagination

--- index.php
+++ index.php
@@ -1,29 +1,34 @@
- 
-<?php get_header(); ?>
+
+<?php get_header();?>
 
 		<div id="base-wrapper">
 			<div id="posts">
-				<?php if(have_posts()) while(have_posts()) { the_post(); ?>
+				<?php if (have_posts()) {
+	while (have_posts()) {the_post();?>
 					<article class="post">
 						<header class="post-header">
-							<h1 class="post-title"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h1>
+							<h1 class="post-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h1>
 						</header>
 						<div class="post-content">
 							<?php the_content('ادامه', 'asdf');?>
-						</div> 
+						</div>
 						<span class="post-date"><?php the_date('l، j F Y')?></span>
 						|
-						<span class="categories"><?php the_category('، '); ?></span>
+						<span class="categories"><?php the_category('، ');?></span>
 					</article>
-				<?php } ?>
+				<?php }
+}
+?>
 			</div>
 
+			<?php if (get_next_posts_link() || get_previous_posts_link()) { // check if there is next of previous posts show pagination ?>
 			<div id="pagination">
 				<h3 id="next"><?php next_posts_link('صفحه بعد');?></h3>
 				<h3>&nbsp;</h3>
 				<h3 id="previous"><?php previous_posts_link('صفحه قبل');?></h3>
-			</div> 
+			</div>
+			<?php }?>
 		</div>
 
 
-<?php get_footer(); ?>
+<?php get_footer();?>
