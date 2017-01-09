7fdf70b95adc713e16540a40d8f12c0e14a806ed
Yahya SayadArbabi <yahya.arbabi@gmail.com>
2017-01-09 23:06:51 +0330

    added GitHub repo link

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 <?php
-	if('footer.php' == basename($_SERVER['SCRIPT_FILENAME']))
-		die ('Please do not load this page directly. Thanks! :)');
+if ('footer.php' == basename($_SERVER['SCRIPT_FILENAME'])) {
+	die('Please do not load this page directly. Thanks! :)');
+}
 ?>
-
 	<div id="footer">
-		<p>The <a href="#">Minimalist</a> theme (v=0.5), design and develop by <a href="http://ysarbabi.ir/">@ysarbabi</a></p>
+		<p>The <a target="blank" href="https://github.com/ysarbabi/minimalist">Minimalist</a> theme (v=0.5), design and develop by <a href="http://ysarbabi.ir/">@ysarbabi</a></p>
 	</div>
 </div>
-<?php wp_footer(); ?>
+<?php wp_footer();?>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery.min.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/base.js"></script>
 </body>
